cc1e36208cabd2e505e3a1549cb162a751b1bfb8
Dr. Zane Wubbena <zwubbena@gmail.com>
2020-06-29 10:37:55 -0500

    Update blm.py

--- blm.py
+++ blm.py
@@ -1,8 +1,11 @@
 
-# Black population in United States is 47,841,851 (14.6%)
-# https://blackdemographics.com/
+# Black population in United States is 47,841,851 (14.6%) https://blackdemographics.com/
+# George Floyd (a Black man) was killed by Derek Chauvin (a Minneapolis, MN police officer)
+# Derek Chauvin pressed his knee on George Floyd's neck for approximatly 8 min and 46 s
+# The exact time has been disputed - but, that's just academic https://www.nytimes.com/2020/06/18/us/george-floyd-timing.html?auth=login-email&login=email
+# The 8 min and 46 s is exactly 526 seconds
 
-for i in range(47841851):
+for EachSecond in range(526):
     print("Black Lives Matter")
 
     
